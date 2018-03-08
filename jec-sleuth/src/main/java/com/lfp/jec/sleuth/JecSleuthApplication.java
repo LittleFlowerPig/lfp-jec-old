1f18b848c53b4bdacc9ab83d77499fefdcc2335d
zhutao0616 <zhutao0616@gmail.com>
2018-03-08 09:26:57 +0800

    链路追踪-完善内容

--- jec-sleuth/src/main/java/com/lfp/jec/sleuth/JecSleuthApplication.java
+++ jec-sleuth/src/main/java/com/lfp/jec/sleuth/JecSleuthApplication.java
@@ -2,9 +2,12 @@ package com.lfp.jec.sleuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+//微服务 - 注册服务
+@EnableEurekaClient
 //链路追踪 - 服务端
 @EnableZipkinServer
 public class JecSleuthApplication {
